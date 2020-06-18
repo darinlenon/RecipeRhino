b03785994b0261248968bd558550d51e0812f9ba
darinlenon <dlenonsemail@gmail.com>
2020-06-18 13:31:57 -0400

    uploaded new ids

--- js/recipes-add.js
+++ js/recipes-add.js
@@ -13,18 +13,29 @@ if (recipe_arr.length) {
 
 
     // Add other info
-    $("#total_time").html(rec.total_time);
+    $("#time_total").html(rec.time_total);
+    $("#time_type").html(rec.time_type);
+    $("#time_label").html(rec.time_label);
     $("#yield").html(rec.yield);
+    $("#yield_type").html(rec.yield_type);
+    $("#yield_label").html(rec.yield_label);
     $("#prep_time").html(rec.prep_time);
+    $("#prep_type").html(rec.prep_type);
+    $("#prep_label").html(rec.prep_label);
     $("#cook_time").html(rec.cook_time);
-    $("#temp").html(rec.temp + '°');
+    $("#cook_type").html(rec.cook_type);
+    $("#cook_label").html(rec.cook_label);
+    $("#temp").html(rec.temp);
+    $("#temp_alt").html(rec.temp_alt);
+    $("#temp_label").html(rec.temp_label);
 
     $("#skill_level").html(rec.skill_level);
     $("#cuisine_text").html(rec.cuisine);
     $("#course_text").html(rec.course);
 
+    $("#recipe_blurb").html(rec.recipe_blurb);
 
-    // Set up ingredients
+    // Set up steps
     var ingredient_html_list = $("#ingredients_list");
     let ingredients = rec.ingredients;
 
@@ -41,17 +52,53 @@ if (recipe_arr.length) {
 
 
     // Set up steps
-    var steps_html_list = $("#steps_list");
-    let steps = rec.steps;
+    var instructions_html_list = $("#instructions_list");
+    let instructions = rec.instructions;
 
-    for(var i = 0; i < steps.length; i++) {
-        steps_html_list.append( 
+    for(var i = 0; i < instructions.length; i++) {
+        instructions_html_list.append( 
             '<div class="step-single">' +
                 '<div class="step-detail">' +
                     '<h3>Step ' + (i+1) + '.</h3>' +
-                    '<p>' + steps[i] + '</p>' + 
+                    '<p>' + instructions[i] + '</p>' + 
                 '</div>' +
             '</div>' 
+        );
+    };
+
+    $("#serving_size").html(rec.serving_size);
+    $("#total_servings").html(rec.total_servings);
+    $("#calories").html(rec.calories);
+    $("#carbohydrates").html(rec.carbohydrates + 'g');
+    $("#total_fat").html(rec.total_fat + 'g');
+    $("#sodium").html(rec.sodium + 'mg');
+    $("#protein").html(rec.protein + 'g');
+    $("#sugar").html(rec.sugar + 'g');
+
+    // Set up steps
+    var tips_var_html_list = $("#tips_var_list");
+    let tips_var = rec.tips_var;
+
+    for(var i = 0; i < tips_var.length; i++) {
+        tips_var_html_list.append( 
+            '<li>' +
+                '<p>' +
+                    tips_var[i] +
+                '</p>' +
+            '</li>' 
+        );
+    };
+
+
+    // Set up steps
+    var tags_html_list = $("#tags_list");
+    let tags = rec.tags;
+
+    for(var i = 0; i < tags.length; i++) {
+        tags_html_list.append( 
+            '<li class="tag-boxes">' +
+                tags[i] +
+            '</li>'
         );
     };
 
